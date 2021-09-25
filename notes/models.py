4ebe30d0bd1ae135d565605181f3d398dd8a6913
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-09-25 17:16:26 -0300

    adding tag column

--- notes/models.py
+++ notes/models.py
@@ -4,8 +4,19 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    tags = models.ManyToManyField('Tag', through="Tagging")
 
     def __str__(self):
         s = (f'{self.id}. {self.title}')
         return s
-    
+
+class Tagging(models.Model):
+    notes = models.ForeignKey('Note', on_delete=models.CASCADE)
+    taggings = models.ForeignKey('Tag', on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        s = (f'{self.id}. {self.tag_name}')
+        return s
