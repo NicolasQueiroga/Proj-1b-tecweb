9ec253b3b2dc1bbb68319de1546f76479ea13780
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-09-25 17:16:42 -0300

    update url

--- notes/urls.py
+++ notes/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('upload/', views.upload, name='upload-note'),
+    path('upload/', views.upload_note, name='upload-note'),
     path('update/<int:note_id>', views.update_note),
     path('delete/<int:note_id>', views.delete_note)
 ]
