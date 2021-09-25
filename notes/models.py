6fe6426c9f28ef4f69005281809684e4df3f7d26
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-09-25 18:36:23 -0300

    adicionando tags

--- notes/models.py
+++ notes/models.py
@@ -4,19 +4,15 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    tags = models.ManyToManyField('Tag', through="Tagging")
+    tag = models.ForeignKey('Tag', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         s = (f'{self.id}. {self.title}')
         return s
 
-class Tagging(models.Model):
-    notes = models.ForeignKey('Note', on_delete=models.CASCADE)
-    taggings = models.ForeignKey('Tag', on_delete=models.CASCADE)
 
 class Tag(models.Model):
-    tag_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
-        s = (f'{self.id}. {self.tag_name}')
-        return s
+        return self.name
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
 from .models import Note, Tag
-from .forms import NoteCreate
-from django.http import HttpResponse
 
 
 def index(request):
@@ -11,32 +9,27 @@ def index(request):
 
 
 def upload_note(request):
-    upload_note = NoteCreate()
     if request.method == 'POST':
-        upload_note = NoteCreate(request.POST, request.FILES)
+        note = Note()
+        note.title = request.POST.get('title')
+        note.content = request.POST.get('content')
         tag_name = request.POST.get('tag')
         tag, create = Tag.objects.get_or_create(name=tag_name)
         if create:
             tag.save()
-        upload_note.tags = tag
-        if upload_note.is_valid():
-            upload_note.save()
-            return redirect('index')
-        else:
-            return HttpResponse("""your form is wrong, reload <a href = "{{ url : 'index'}}">here</a>""")
+        note.tag = tag
+
+        note.save()
+        return redirect('index')
 
 
 def update_note(request, note_id):
-    note_id = int(note_id)
-    try:
-        note_sel = Note.objects.get(id=note_id)
-    except Note.DoesNotExist:
-        return redirect('index')
-    note_form = NoteCreate(request.POST or None, instance=note_sel)
-    if note_form.is_valid():
-        note_form.save()
-        return redirect('index')
-    return render(request, 'notes/index.html', {'upload_form': note_form})
+    tag_name = request.POST.get('tag')
+    tag, create = Tag.objects.get_or_create(name=tag_name)
+    if create:
+        tag.save()
+    Note.objects.filter(pk=note_id).update(title=request.POST.get('title'), content=request.POST.get('content'), tag=tag) 
+    return redirect('index')
 
 
 def delete_note(request, note_id):
