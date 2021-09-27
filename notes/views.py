ac0dabf77cc93357678d5736aa28d71fe3fbba52
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-09-26 21:36:40 -0300

    tags ok!

--- notes/views.py
+++ notes/views.py
@@ -6,9 +6,11 @@ def index_note(request):
     notes = Note.objects.all()
     return render(request, 'notes/notes.html', {'notes': notes})
 
+
 def index_tag(request):
     tags = Tag.objects.all()
     return render(request, 'tags/tags.html', {'tags': tags})
+
 
 def sorted_tag(request, tag_id):
     tag = Tag.objects.get(pk=tag_id)
@@ -22,10 +24,11 @@ def upload_note(request):
         note.title = request.POST.get('title')
         note.content = request.POST.get('content')
         tag_name = request.POST.get('tag')
-        tag, create = Tag.objects.get_or_create(name=tag_name)
-        if create:
-            tag.save()
-        note.tag = tag
+        if tag_name != '':
+            tag, create = Tag.objects.get_or_create(name=tag_name)
+            if create:
+                tag.save()
+            note.tag = tag
 
         note.save()
         return redirect('index')
@@ -33,10 +36,19 @@ def upload_note(request):
 
 def update_note(request, note_id):
     tag_name = request.POST.get('tag')
-    tag, create = Tag.objects.get_or_create(name=tag_name)
-    if create:
-        tag.save()
-    Note.objects.filter(pk=note_id).update(title=request.POST.get('title'), content=request.POST.get('content'), tag=tag) 
+    note_sel = Note.objects.get(id=note_id)
+    if note_sel.tag:
+        tag_id = note_sel.tag.id
+        if Tag.objects.filter(pk=note_sel.tag.id):
+            Tag.objects.get(id=tag_id).delete()
+    if tag_name != '':
+        tag, create = Tag.objects.get_or_create(name=tag_name)
+        if create:
+            tag.save()
+        Note.objects.filter(pk=note_id).update(title=request.POST.get('title'), content=request.POST.get('content'), tag=tag)
+    else:
+        Note.objects.filter(pk=note_id).update(title=request.POST.get('title'), content=request.POST.get('content'))
+    
     return redirect('index')
 
 
@@ -46,8 +58,14 @@ def delete_note(request, note_id):
         note_sel = Note.objects.get(id=note_id)
     except Note.DoesNotExist:
         return redirect('index')
+    if note_sel.tag:
+        tag_id = note_sel.tag.id
+        if Tag.objects.filter(pk=note_sel.tag.id):
+            Tag.objects.get(id=tag_id).delete()
     note_sel.delete()
+
     return redirect('index')
+
 
 def delete_tag(request, tag_id):
     tag_id = int(tag_id)
@@ -57,4 +75,3 @@ def delete_tag(request, tag_id):
         return redirect('index')
     tag_sel.delete()
     return redirect('tags')
-
